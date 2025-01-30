ff76df55e97fa6a142959f35750ee2dd4d706a38
shanon cymon <shanonsimon082@gmail.com>
2025-01-30 09:27:01 +0300

    first commit

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-    //this is a comment
-    echo "Hello world";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
 
-?>
+<body>
+    
+    <?php
+    //this is a comment in php
+    echo " this is php class";
+    ?>
+
+<h1>hello <?php echo "world"; ?></h1>
+<p>this is all about php <?php echo "Let's do this"; ?></p>
+
+</body>
+
+</html>
